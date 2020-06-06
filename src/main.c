22118abbffb5d1723e1dc4b48313e1b076d6aed2
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2020-06-06 13:07:14 -0500

    TODO fill these out and make it not suck

--- src/main.c
+++ src/main.c
@@ -10,6 +10,70 @@
 #include "mpu_9250.h"
 
 
+float get_accelerometer_conversion_factor(
+    const int accelerometer_data_rate
+    )
+{
+  return 0.0f;
+}
+
+
+int get_accelerometer_data_rate(
+    const int accelerometer_data_rate_requested
+    )
+{
+  return 0;
+}
+
+
+float get_gyroscope_conversion_factor(
+    const int gyroscope_data_rate
+    )
+{
+  return 0.0f;
+}
+
+
+int get_gyroscope_data_rate(
+    const int gyroscope_data_rate_requested
+    )
+{
+  return 0;
+}
+
+
+void setup_mpu_9250(
+      float* accelerometer_conversion_factor
+    , float* gyroscope_conversion_factor
+    , const int fd
+    , const int gyroscope_data_rate
+    , const int accelerometer_data_rate
+    )
+{
+  const int _a = get_accelerometer_data_rate(accelerometer_data_rate);
+  const int _g = get_gyroscope_data_rate(gyroscope_data_rate);
+
+  const uint8_t config_buffer1[] =
+  {
+      MPU_9250_REG_GYRO_CONFIG
+    , MPU_9250_WRITE_GYRO_CONFIG(0, 0, 0, 0b11, 0) // 2000 dps
+    , MPU_9250_WRITE_ACCEL_CONFIG_1(0, 0, 0, 0b11) // 16g
+  };
+
+  const uint8_t config_buffer2[] =
+  {
+      MPU_9250_REG_PWR_MGMT_1
+    , MPU_9250_WRITE_PWR_MGMT_1(0, 0, 0, 0, 0, 1)
+  };
+
+  write(fd, config_buffer1, sizeof(config_buffer1));
+  write(fd, config_buffer2, sizeof(config_buffer2));
+
+  *accelerometer_conversion_factor = get_accelerometer_conversion_factor(_a);
+  *gyroscope_conversion_factor = get_gyroscope_conversion_factor(_a);
+}
+
+
 int main(int argc, char** argv)
 {
 	const char* device_path = "/dev/i2c-1";
@@ -19,26 +83,6 @@ int main(int argc, char** argv)
   // the sensor allows continuous writes and it automatically increments registers
   // exploit this fact to do a gigantic singular write call to config the sensor
   // pick the first register we want to configure, and then configure everything afterward
-  {
-    const uint8_t config_buffer[] =
-    {
-        MPU_9250_REG_GYRO_CONFIG
-      , MPU_9250_WRITE_GYRO_CONFIG(0, 0, 0, 0b11, 0) // 2000 dps
-      , MPU_9250_WRITE_ACCEL_CONFIG_1(0, 0, 0, 0b11) // 16g
-    };
-
-    write(fd, config_buffer, sizeof(config_buffer));
-  }
-
-  {
-    const uint8_t config_buffer[] =
-    {
-        MPU_9250_REG_PWR_MGMT_1
-      , MPU_9250_WRITE_PWR_MGMT_1(0, 0, 0, 0, 0, 1)
-    };
-
-    write(fd, config_buffer, sizeof(config_buffer));
-  }
 
   while(true)
   {
@@ -54,14 +98,6 @@ int main(int argc, char** argv)
     const int16_t gyro_xout  = (read_buffer[ 8] << 8) | read_buffer[ 9];
     const int16_t gyro_yout  = (read_buffer[10] << 8) | read_buffer[11];
     const int16_t gyro_zout  = (read_buffer[12] << 8) | read_buffer[13];
-
-    //const int32_t temp       = (read_buffer[ 6] << 8) | read_buffer[ 7];
-
-    //for(size_t i = 0; i < sizeof(read_buffer); i ++)
-    //{
-    //  printf("[%zu]:%.2X,", i, read_buffer[i]);
-    //}
-    //puts("");
 
     printf(
           "%f,%f,%f,%f,%f,%f\n"
